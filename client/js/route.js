2a537ba03a7c20a58fc5088f553d653595665926
Ryan Kramer <rkramer613@gmail.com>
2017-04-17 13:30:31 -0600

    UI Routing fixed!

--- client/js/route.js
+++ client/js/route.js
@@ -1,3 +1,23 @@
 
 // UI Routing setup
 
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: '../views/home/home.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit       
+        });
+        
+});
